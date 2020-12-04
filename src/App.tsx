7ed981d8b9520625d6265885b6eec435de5958d7
Spencer Kim <smk95smk@gmail.com>
2020-12-03 21:21:58 -0800

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div className="App">Start Here</div>;
+    return <div className="App">Start Here</div>;
 };
 
 export default App;
